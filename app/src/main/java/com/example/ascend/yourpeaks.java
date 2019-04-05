f056dc6bdcc05919d7894ca72df08836b808e254
Jordan Cohen <jordancohen@Jordans-MacBook-Pro.local>
2019-04-05 10:28:22 -0400

    j stuff in

--- app/src/main/java/com/example/ascend/yourpeaks.java
+++ app/src/main/java/com/example/ascend/yourpeaks.java
@@ -1,4 +1,4 @@
-package com.example.firstsprint;
+package com.example.ascend;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class yourpeaks extends AppCompatActivity {
 
     private TextView mTextMessage;
 
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_yourpeaks);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
