78562ada2b2dee4e5d8a229872cf439ad59d1e4a
Udit Sharma <uditsharma@Udits-MacBook-Pro.local>
2019-04-10 19:26:53 -0400

    got code to compile, bottom navigation crashes

--- app/src/main/java/com/example/ascend/Phase.java
+++ app/src/main/java/com/example/ascend/Phase.java
@@ -1,32 +1,56 @@
 package com.example.ascend;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Vector;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 
 public class Phase extends RealmObject {
-    private Vector<Vector<Pitch>> weeklyPitches;
-    private GregorianCalendar start;
-    private GregorianCalendar end;
+    private RealmList<Pitch> sunday;
+    private RealmList<Pitch> monday;
+    private RealmList<Pitch> tuesday;
+    private RealmList<Pitch> wednesday;
+    private RealmList<Pitch> thursday;
+    private RealmList<Pitch> friday;
+    private RealmList<Pitch> saturday;
+    private Date start;
+    private Date end;
 
-    Phase(GregorianCalendar s, GregorianCalendar e) {
-        this.weeklyPitches = new Vector<Vector<Pitch>>();
-        Vector<Pitch> cur;
-        for (int i = 0; i < 7; i++) {
-            cur = new Vector<Pitch>();
-            this.weeklyPitches.add(cur);
-        }
+    public Phase() {
+        super();
+    }
+
+    public Phase(Date s, Date e) {
+
         this.start = s;
         this.end = e;
     }
 
-    public Vector<Pitch> getPitches(int day) {
-        return weeklyPitches.get(day - 1);
+    public RealmList<Pitch> getPitches(int day) {
+        switch(day) {
+            case 0: return sunday;
+            case 1: return monday;
+            case 2: return tuesday;
+            case 3: return wednesday;
+            case 4: return thursday;
+            case 5: return friday;
+            case 6: return saturday;
+            default: return null;
+        }
     }
     public void addPitch(Pitch p) {
-        Vector<Pitch> pitch = weeklyPitches.get(p.getDay());
-        this.weeklyPitches.add(pitch);
+        int day = p.getDay();
+        switch(day) {
+            case 0: sunday.add(p);
+            case 1: monday.add(p);
+            case 2: tuesday.add(p);
+            case 3: wednesday.add(p);
+            case 4: thursday.add(p);
+            case 5: friday.add(p);
+            case 6: saturday.add(p);
+            default: break;
+        }
     }
 }
