e1e068c7982426df50acbe8a3175af566c6e38c3
Jordan Cohen <jordancohen@Jordans-MacBook-Pro.local>
2019-04-11 18:31:09 -0400

    adding navigation and recycler views with amrit

--- app/src/main/java/com/example/ascend/RecyclerViewAdapterPhase.java
+++ app/src/main/java/com/example/ascend/RecyclerViewAdapterPhase.java
@@ -1,6 +1,7 @@
 package com.example.ascend;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
@@ -8,18 +9,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class RecyclerViewAdapter2 extends  RecyclerView.Adapter<RecyclerViewAdapter2.ViewHolder> {
+public class RecyclerViewAdapterPhase extends  RecyclerView.Adapter<RecyclerViewAdapterPhase.ViewHolder> {
     private static final String TAG = "RecyclerViewAdapter";
 
     private ArrayList<Phase> phases;
     private Context nContext;
 
-    public RecyclerViewAdapter2(ArrayList<Phase> p, Context c) {
+    public RecyclerViewAdapterPhase(ArrayList<Phase> p, Context c) {
         phases = p;
         nContext = c;
     }
@@ -37,6 +37,14 @@ public class RecyclerViewAdapter2 extends  RecyclerView.Adapter<RecyclerViewAdap
         Log.d(TAG, "onBindViewHolder: called");
         Phase p = phases.get(i);
         viewHolder.phase.setText(p.getName());
+
+        viewHolder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), phasetasks.class);
+                nContext.startActivity(i);
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/ascend/peak_phases.java
+++ app/src/main/java/com/example/ascend/peak_phases.java
@@ -1,15 +1,29 @@
 package com.example.ascend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class peak_phases extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    private static final String TAG = "peak_phases";
+    private ArrayList<Phase> curPhases;
+    private String peakname;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,9 +50,47 @@ public class peak_phases extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peak_phases);
 
+        Intent i = getIntent();
+        peakname = i.getStringExtra("peakname");
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Realm.init(this);
+        Realm realm = Realm.getDefaultInstance();
+        curPhases = new ArrayList<Phase>();
+        initCurPeaks();
+    }
+
+
+    public void back(View v) {
+        Intent i = new Intent(peak_phases.this, YourPeaks.class);
+        startActivity(i);
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: called");
+        RecyclerView recycler = findViewById(R.id.listOfPeaks);
+        RecyclerViewAdapterPhase adapter = new RecyclerViewAdapterPhase(curPhases, this);
+        recycler.setAdapter(adapter);
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void initCurPeaks() {
+        Log.d(TAG, "initCurPitches: called");
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<Peak> p = realm.where(Peak.class).equalTo("name", peakname).findAll();
+        Peak peaky = p.get(0);
+        for (int i = 0; i < peaky.phase.size(); i++) {
+            Phase cur = peaky.phase.get(i);
+            if (cur != null) {
+                curPhases.add(cur);
+            }
+        }
+        if (curPhases.size() > 0 ) {
+            initRecyclerView();
+        }
     }
 
 }
