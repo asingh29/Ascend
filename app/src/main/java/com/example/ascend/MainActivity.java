dd59500b8fa8e134375d2dac5b5ffd0920f7e540
Udit Sharma <usharma6@jhu.edu>
2019-04-11 19:34:03 -0400

    made recycle views look better

--- app/src/main/java/com/example/ascend/MainActivity.java
+++ app/src/main/java/com/example/ascend/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Realm.init(this);
         RealmConfiguration config = new RealmConfiguration.Builder()
                 .deleteRealmIfMigrationNeeded()
