13aea9d26304adedc52276c81dc39a8723595c5f
Amritpal Singh <amritsunner29@gmail.com>
2019-04-11 15:01:54 -0400

    different recyclerviews

--- app/src/main/java/com/example/ascend/RecyclerViewAdapter2.java
+++ app/src/main/java/com/example/ascend/RecyclerViewAdapter2.java
@@ -1,4 +1,57 @@
 package com.example.ascend;
 
-public class RecyclerViewAdapter2 {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter2 extends  RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<Phase> phases;
+    private Context nContext;
+
+    public RecyclerViewAdapter2(ArrayList<Phase> p, Context c) {
+        phases = p;
+        nContext = c;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.phases_layout, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int i) {
+        Log.d(TAG, "onBindViewHolder: called");
+        Phase p = phases.get(i);
+        viewHolder.name.setText(p.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return phases.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView phase;
+        ConstraintLayout layout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            phase = itemView.findViewById(R.id.phase);
+            layout = itemView.findViewById(R.id.phase_layout);
+        }
+    }
 }
--- app/src/main/java/com/example/ascend/RecyclerViewAdapter3.java
+++ app/src/main/java/com/example/ascend/RecyclerViewAdapter3.java
@@ -1,4 +1,59 @@
 package com.example.ascend;
 
-public class RecyclerViewAdapter3 {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter3 extends  RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<Peak> peaks;
+    private Context nContext;
+
+    public RecyclerViewAdapter3(ArrayList<Peak> p, Context c) {
+        peaks = p;
+        nContext = c;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.phases_layout, viewGroup, false);
+        ViewHolder Holder = new ViewHolder(view);
+        return Holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int i) {
+        Log.d(TAG, "onBindViewHolder: called");
+        Peak p = peaks.get(i);
+        viewHolder.name.setText(p.getName());
+        viewHolder.time.setText(p.getStart().toString() + "" + p.getEnd().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return peaks.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        TextView time;
+        ConstraintLayout layout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            time = itemView.findViewById(R.id.time);
+            layout = itemView.findViewById(R.id.peaks_layout);
+        }
+    }
 }
