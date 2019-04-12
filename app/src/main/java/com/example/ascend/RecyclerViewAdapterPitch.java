cfe9f6b853b32ccb7c1a8b5dc7d007fe239405d1
Amritpal Singh <amritsunner29@gmail.com>
2019-04-11 22:39:10 -0400

    worked on more recylerviews

--- app/src/main/java/com/example/ascend/RecyclerViewAdapterPitch.java
+++ app/src/main/java/com/example/ascend/RecyclerViewAdapterPitch.java
@@ -1,7 +1,6 @@
 package com.example.ascend;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
@@ -10,49 +9,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class RecyclerViewAdapterPeak extends  RecyclerView.Adapter<RecyclerViewAdapterPeak.ViewHolder> {
+public class RecyclerViewAdapterPitch  extends  RecyclerView.Adapter<RecyclerViewAdapterPitch.ViewHolder> {
+
     private static final String TAG = "RecyclerViewAdapter";
 
-    private ArrayList<Peak> peaks;
+    private ArrayList<Pitch> pitches;
     private Context nContext;
 
-    public RecyclerViewAdapterPeak(ArrayList<Peak> p, Context c) {
-        peaks = p;
+    public RecyclerViewAdapterPitch(ArrayList<Pitch> p, Context c) {
+        pitches = p;
         nContext = c;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.peaks_layout, viewGroup, false);
-        ViewHolder Holder = new ViewHolder(view);
-        return Holder;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.phasetasks_layout, viewGroup, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         Log.d(TAG, "onBindViewHolder: called");
-        final Peak p = peaks.get(i);
+        final Pitch p = pitches.get(i);
         viewHolder.name.setText(p.getName());
-        viewHolder.time.setText(p.getStart().toString() + "" + p.getEnd().toString());
-
-        viewHolder.layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(v.getContext(), peak_phases.class);
-                i.putExtra("peakname", p.getName());
-                nContext.startActivity(i);
-            }
-        });
+        viewHolder.time.setText(p.getStart() + "-" + p.getEnd());
     }
+
 
     @Override
     public int getItemCount() {
-        return peaks.size();
+        return pitches.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -64,7 +55,8 @@ public class RecyclerViewAdapterPeak extends  RecyclerView.Adapter<RecyclerViewA
             super(itemView);
             name = itemView.findViewById(R.id.name);
             time = itemView.findViewById(R.id.time);
-            layout = itemView.findViewById(R.id.peaks_layout);
+            layout = itemView.findViewById(R.id.pitch_layout);
         }
     }
+
 }
