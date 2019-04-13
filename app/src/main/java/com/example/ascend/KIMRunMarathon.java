f7c1c5b45bac66b0f114ba47465d9e02f4a13ada
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-13 12:29:29 -0400

    Bumjin Kim (bkim63) Next sprint: floating button action requires Create Your Own Peak screen which will be done in next sprint implementation

--- app/src/main/java/com/example/ascend/KIMRunMarathon.java
+++ app/src/main/java/com/example/ascend/KIMRunMarathon.java
@@ -50,7 +50,7 @@ public class KIMRunMarathon extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Please wait for 2nd Sprint", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
