da17ee702ef26c5acba06ece607fe0008ed43c9e
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-13 02:27:33 -0400

    Bumjin Kim (bkim63) added bottom navigation view to rise together, run marathon, your peaks screens.

--- app/src/main/java/com/example/ascend/KIMRiseTogether.java
+++ app/src/main/java/com/example/ascend/KIMRiseTogether.java
@@ -6,12 +6,34 @@ package com.example.ascend;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class KIMRiseTogether extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Intent i = new Intent(KIMRiseTogether.this, MainActivity.class);
+                    startActivity(i);
+                    return true;
+                case R.id.navigation_peaks:
+                    return true;
+                case R.id.navigation_browse:
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +41,10 @@ public class KIMRiseTogether extends AppCompatActivity {
         setContentView(R.layout.activity_kim_rise_together);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_browse);
 
         Button learnSkillButton = (Button)findViewById(R.id.learnSkillButton);
         learnSkillButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/ascend/KIMRunMarathon.java
+++ app/src/main/java/com/example/ascend/KIMRunMarathon.java
@@ -4,14 +4,36 @@
  */
 package com.example.ascend;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class KIMRunMarathon extends AppCompatActivity {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    Intent i = new Intent(KIMRunMarathon.this, MainActivity.class);
+                    startActivity(i);
+                    return true;
+                case R.id.navigation_peaks:
+                    return true;
+                case R.id.navigation_browse:
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +41,10 @@ public class KIMRunMarathon extends AppCompatActivity {
         setContentView(R.layout.activity_kim_run_marathon);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_browse);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
