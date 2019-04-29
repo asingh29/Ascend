53b260e13993888887fcf0f6a1696e1496a000fe
Amritpal Singh <amritsunner29@gmail.com>
2019-04-29 16:30:16 -0400

    worked on deleting peaks, phases, and pitches

--- app/src/main/java/com/example/ascend/phasetasks.java
+++ app/src/main/java/com/example/ascend/phasetasks.java
@@ -176,8 +176,14 @@ public class phasetasks extends AppCompatActivity implements DatePickerDialog.On
     public void onCLick(View v) {
         Realm realm = Realm.getDefaultInstance();
         realm.beginTransaction();
-        Phase phasey = realm.where(Phase.class).equalTo("name", phasename).findFirst();
-        phasey.deleteFromRealm();
+        Phase p = realm.where(Phase.class).equalTo("name", phasename).findFirst();
+        RealmList<Pitch> phasey = p.all;
+        Log.d(TAG, "initCurPitches: " + phasey.size());
+        for (int i = 0; i < phasey.size(); i++) {
+            Pitch cur = phasey.get(i);
+            cur.deleteFromRealm();
+        }
+        p.deleteFromRealm();
         realm.commitTransaction();
         Intent i = new Intent(phasetasks.this, peak_phases.class);
         i.putExtra("peakname", peakname);
